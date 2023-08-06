0390efd2cef10a6f4427426b5eee2ef3dbf5be84
mubahilll <101805477+mubahilll@users.noreply.github.com>
2023-08-06 16:57:09 +0500

    Add files via upload

---
+++ decorators.py
@@ -0,0 +1,53 @@
+from datetime import datetime
+import time
+
+def logger(func):
+    def wrapper():
+        print("-"*50)
+        print("> Execution started {}".format(datetime.today().strftime("%Y-%m-%d %H:%M:%S")))
+        func()
+        print("> Execution completed {}".format(datetime.today().strftime("%Y-%m-%d %H:%M:%S")))
+        print("-"*50)
+    return wrapper
+
+@logger
+def demo_funct():
+    print("Executing task")
+    time.sleep(3)
+    print("Task completed!")
+
+# demo_funct()
+
+# logger(demo_funct())
+
+def logger_args(func):
+    def wrapper(*args, **kwargs):
+        print("-"*50)
+        print("> Execution started {}".format(datetime.today().strftime("%Y-%m-%d %H:%M:%S")))
+        func(*args, **kwargs)
+        print("> Execution completed {}".format(datetime.today().strftime("%Y-%m-%d %H:%M:%S")))
+        print("-"*50)
+    return wrapper
+
+@logger_args
+def demo_func_args(sleep_time):
+    print("Executing task")
+    time.sleep(sleep_time)
+    print("Task completed!")
+
+
+demo_func_args(3)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ generators.py
@@ -0,0 +1,50 @@
+#rather than terminating function by return statement, we can pause the execution of func by storing th
+#generators provide a simple way to create iterators
+
+
+def demo():
+    n = 1
+    yield n
+
+    n += 1
+    yield n
+
+    n += 1
+    yield n
+
+
+test = demo()
+print(test)
+print(next(test))
+print(next(test))
+print(next(test))
+
+print("-"*10)
+test2 = demo()
+
+for a in test2:
+    print(a)
+
+
+def xor_static_key(a):
+    key = 0x5
+    for i in a:
+        yield chr(ord(i) ^ key) 
+
+
+for i in xor_static_key("test"):
+    print(i)
+
+
+
+
+
+xor_static_key2 = (chr(ord(i) ^ 0x5) for i in "test") #anonymous generator expression
+# print(xor_static_key2)
+print("*"*10)
+for i in xor_static_key2:
+    print(i)
+
+
+
+
