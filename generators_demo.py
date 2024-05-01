ccb11aaa06890de74af6088ecb7fa920e819ec38
mubahilll <101805477+mubahilll@users.noreply.github.com>
2024-05-01 18:10:48 +0500

    Update and rename generators.py to generators_demo.py

--- generators_demo.py
+++ generators_demo.py
@@ -1,7 +1,6 @@
 #rather than terminating function by return statement, we can pause the execution of func by storing th
 #generators provide a simple way to create iterators
 
-
 def demo():
     n = 1
     yield n
@@ -36,15 +35,8 @@ for i in xor_static_key("test"):
     print(i)
 
 
-
-
-
 xor_static_key2 = (chr(ord(i) ^ 0x5) for i in "test") #anonymous generator expression
 # print(xor_static_key2)
 print("*"*10)
 for i in xor_static_key2:
     print(i)
-
-
-
-
